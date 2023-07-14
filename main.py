c32c0d43c6714c000425c9d8b99d9c3223856e71
MasterJLord <jayden.cammarata@gmail.com>
2023-07-14 00:38:28 -0700

    QOL change and a couple more customization options
    Multiple presets can now coexist and write to different csv files
    Coyote time
    Indicator lines
    Customizeable speed animation

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from operator import mod
+from tkinter import Frame
 import pygame, sys, json, random, math
 
 clock = pygame.time.Clock()
 finalscreen = pygame.display.set_mode((1500, 1000))
 intermediatescreen = pygame.Surface((1500, 1000), pygame.SRCALPHA)
 
-settingsTemp = json.loads(open("jsons/config.json", "r").read())
+settingsTemp = json.loads(open("presets/control.json", "r").read())
 class Blank:
     def __init__(self, information: dict):
+        self.OutputFile = information["OutputFile"]
         self.PixelsPerUnit=information["PixelsPerUnit"]
         self.PlayerWidth = information["PlayerWidth"]
         self.PlayerHeight = information["PlayerHeight"]
         self.BackgroundGridSize=information["BackgroundGridSize"]
         self.FloorHeight=information["FloorHeight"]
+        self.IndicatorUptime = information["IndicatorUptime"]
         self.SpeedAnimation=information["SpeedAnimation"]
         self.HorizontalAcceleration = information["HorizontalAcceleration"]
         self.HorizontalDragFlat = information["HorizontalDragFlat"]
         self.HorizontalMaxSpeed = information["HorizontalMaxSpeed"]
+        self.CoyoteTime = information["CoyoteTime"]
         self.JumpVelocity = information["JumpVelocity"]
         self.Gravity=information["Gravity"]
         self.MinimumForgiveness = information["MinimumForgiveness"]
@@ -31,17 +35,19 @@ TotalTime = 0
 Score = 0
 pygame.display.set_caption("Score: 0")
 Deaths = ""
-PlayerCoordinates = [0, Settings.PlayerHeight*2]
+PlayerCoordinates = [0, Settings.PlayerHeight]
 Moving = [0, 0]
 Momentum = [0, 0]
-Platforms = [[Settings.HorizontalMaxSpeed**2/(2*(Settings.HorizontalAcceleration-Settings.HorizontalDragFlat))/-2, -1, Settings.HorizontalMaxSpeed**2/(2*(Settings.HorizontalAcceleration-Settings.HorizontalDragFlat))], [Settings.HorizontalMaxSpeed**2/(2*(Settings.HorizontalAcceleration-Settings.HorizontalDragFlat))/-2, 0, Settings.HorizontalMaxSpeed**2/(2*(Settings.HorizontalAcceleration-Settings.HorizontalDragFlat))]]
+Platforms = [[Settings.HorizontalMaxSpeed**2/(2*(Settings.HorizontalAcceleration-Settings.HorizontalDragFlat))/-2, -0.5*Settings.FloorHeight, Settings.HorizontalMaxSpeed**2/(2*(Settings.HorizontalAcceleration-Settings.HorizontalDragFlat))], [Settings.HorizontalMaxSpeed**2/(2*(Settings.HorizontalAcceleration-Settings.HorizontalDragFlat))/-2, 0, Settings.HorizontalMaxSpeed**2/(2*(Settings.HorizontalAcceleration-Settings.HorizontalDragFlat))]]
 OnGround = 0
+IndicatorTime = 0
+CoyoteTimer = 0
 
 pygame.mixer.init()
 pygame.mixer.music.load("Songs/"+Settings.SongName)
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
-# TODO: Download music (Greenpath, God of of the Dead, Reach for the Summit)
+# TODO: Download music (First Steps, Elite Fight, First Steps)
 try:
 # if True:
     while True:
@@ -58,9 +64,10 @@ try:
                 elif event.key == pygame.K_RIGHT:
                     Moving[1] = 1
                 elif event.key == pygame.K_UP:
-                    if OnGround:
+                    if OnGround or CoyoteTimer > 0:
                         Momentum[1] = Settings.JumpVelocity
                         OnGround = 0
+                        CoyoteTimer = 0
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     Moving[0] = 0
@@ -96,21 +103,23 @@ try:
                     # Moves the platform
                     pygame.display.set_caption("Score: "+str(Score))
                     Score += 1
-                    Platforms.pop(0)
                     temp3 = random.randint(math.floor(Settings.HorizontalMaxSpeed**2/(2*(Settings.HorizontalAcceleration-Settings.HorizontalDragFlat))+Settings.MinimumForgiveness), math.floor(Settings.HorizontalMaxSpeed**2/(2*(Settings.HorizontalAcceleration-Settings.HorizontalDragFlat))+Settings.MaximumForgiveness))
                     # Moves the platform
                     tempa = random.randint(1, math.floor(8*(Settings.JumpVelocity/Settings.Gravity)))/4
                     if tempa < Settings.JumpVelocity/Settings.Gravity:
-                        temp1 = random.randint(math.ceil(Platforms[0][0]-temp3-tempa*Settings.HorizontalMaxSpeed), math.floor(Platforms[0][0]+Platforms[0][2]+tempa*Settings.HorizontalMaxSpeed))
-                        temp2 = random.randint(math.floor(Platforms[0][1]+(Settings.JumpVelocity**2/Settings.Gravity)/4), math.floor(Platforms[0][1]+(Settings.JumpVelocity**2/Settings.Gravity)/2))
+                        temp1 = random.randint(math.ceil(Platforms[1][0]-temp3-tempa*Settings.HorizontalMaxSpeed), math.floor(Platforms[1][0]+Platforms[1][2]+tempa*Settings.HorizontalMaxSpeed))
+                        temp2 = random.randint(math.floor(Platforms[1][1]+(Settings.JumpVelocity**2/Settings.Gravity)/4), math.floor(Platforms[1][1]+(Settings.JumpVelocity**2/Settings.Gravity)/2))
                     else:
-                        temp2 = random.randint(math.floor(Platforms[0][1]+Settings.JumpVelocity*tempa-Settings.Gravity*(tempa**2)/2-Settings.MaximumForgiveness), math.floor(Platforms[0][1]+Settings.JumpVelocity*tempa-Settings.Gravity*(tempa**2)/2-Settings.MinimumForgiveness))
+                        temp2 = random.randint(math.floor(Platforms[1][1]+Settings.JumpVelocity*tempa-Settings.Gravity*(tempa**2)/2-Settings.MaximumForgiveness), math.floor(Platforms[1][1]+Settings.JumpVelocity*tempa-Settings.Gravity*(tempa**2)/2-Settings.MinimumForgiveness))
                         tempb = random.randint(0, 1)
                         if tempb == 0:
-                            temp1 = random.randint(math.ceil(Platforms[0][0]-temp3-tempa*Settings.HorizontalMaxSpeed+Settings.MinimumForgiveness), math.ceil(Platforms[0][0]-temp3-tempa*Settings.HorizontalMaxSpeed+Settings.MaximumForgiveness))
+                            temp1 = random.randint(math.ceil(Platforms[1][0]-temp3-tempa*Settings.HorizontalMaxSpeed+Settings.MinimumForgiveness), math.ceil(Platforms[1][0]-temp3-tempa*Settings.HorizontalMaxSpeed+Settings.MaximumForgiveness))
                         else:
-                            temp1 = random.randint(math.ceil(Platforms[0][0]+Platforms[0][2]+tempa*Settings.HorizontalMaxSpeed-Settings.MaximumForgiveness), math.ceil(Platforms[0][0]+Platforms[0][2]+tempa*Settings.HorizontalMaxSpeed-Settings.MinimumForgiveness))
+                            temp1 = random.randint(math.ceil(Platforms[1][0]+Platforms[1][2]+tempa*Settings.HorizontalMaxSpeed-Settings.MaximumForgiveness), math.ceil(Platforms[1][0]+Platforms[1][2]+tempa*Settings.HorizontalMaxSpeed-Settings.MinimumForgiveness))
                     Platforms.append((temp1, temp2, temp3))
+                    Platforms.pop(0)
+                    IndicatorTime = Settings.IndicatorUptime
+
 
 
 
@@ -123,10 +132,11 @@ try:
             if Momentum[0] < 0:
                 if PlayerCoordinates[0] < Platforms[0][0]:
                     OnGround = 0
+                    CoyoteTimer = Settings.CoyoteTime
             elif Momentum[0] > 0:
                 if PlayerCoordinates[0] > Platforms[0][0]+Platforms[0][2]:
                     OnGround = 0
-
+                    CoyoteTimer = Settings.CoyoteTime
         # Makes player respawn
         if Momentum[1] <= -Settings.RespawnMultiplier*Settings.Gravity:
             Momentum = [0, 0]
@@ -135,7 +145,8 @@ try:
                 Deaths += ";"
             Deaths += str(Score)
 
-
+        if CoyoteTimer > 0:
+            CoyoteTimer -= FrameTime/1000
 
         # Draws background grid
         intermediatescreen.fill((155, 155, 155))
@@ -153,27 +164,42 @@ try:
                 tempb += Settings.BackgroundGridSize*2
             tempa += Settings.BackgroundGridSize
 
+        # Draws indicator lines
+        if IndicatorTime > 0:
+            IndicatorTime -= FrameTime/1000
+            pygame.draw.line(intermediatescreen, (60, 50, 50), ((Platforms[0][0]+Settings.PlayerWidth/2-PlayerCoordinates[0])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[0][1]+Settings.PlayerHeight/2)*Settings.PixelsPerUnit+500), ((Platforms[1][0]+Settings.PlayerWidth/2-PlayerCoordinates[0])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[1][1]+Settings.PlayerHeight/2)*Settings.PixelsPerUnit+500), Settings.PixelsPerUnit)
+            pygame.draw.line(intermediatescreen, (60, 50, 50), ((Platforms[0][0]-Settings.PlayerWidth/2-PlayerCoordinates[0]+Platforms[0][2])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[0][1]+Settings.PlayerHeight/2)*Settings.PixelsPerUnit+500), ((Platforms[1][0]-Settings.PlayerWidth/2-PlayerCoordinates[0]+Platforms[1][2])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[1][1]+Settings.PlayerHeight/2)*Settings.PixelsPerUnit+500), Settings.PixelsPerUnit)
+            pygame.draw.line(intermediatescreen, (60, 50, 50), ((Platforms[0][0]+Settings.PlayerWidth/2-PlayerCoordinates[0])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[0][1]+Settings.PlayerHeight/2+Settings.FloorHeight)*Settings.PixelsPerUnit+500), ((Platforms[1][0]+Settings.PlayerWidth/2-PlayerCoordinates[0])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[1][1]+Settings.PlayerHeight/2+Settings.FloorHeight)*Settings.PixelsPerUnit+500), Settings.PixelsPerUnit)
+            pygame.draw.line(intermediatescreen, (60, 50, 50), ((Platforms[0][0]-Settings.PlayerWidth/2-PlayerCoordinates[0]+Platforms[0][2])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[0][1]+Settings.PlayerHeight/2+Settings.FloorHeight)*Settings.PixelsPerUnit+500), ((Platforms[1][0]-Settings.PlayerWidth/2-PlayerCoordinates[0]+Platforms[1][2])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[1][1]+Settings.PlayerHeight/2+Settings.FloorHeight)*Settings.PixelsPerUnit+500), Settings.PixelsPerUnit)
+
         # Draws platforms
-            pygame.draw.rect(intermediatescreen, (60, 50, 50), ((Platforms[0][0]+Settings.PlayerWidth/2-PlayerCoordinates[0])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[0][1]+Settings.PlayerHeight/2)*Settings.PixelsPerUnit+500, (Platforms[0][2]-Settings.PlayerWidth)*Settings.PixelsPerUnit, Settings.FloorHeight*Settings.PixelsPerUnit))
-            pygame.draw.rect(intermediatescreen, (50, 35, 35), ((Platforms[1][0]+Settings.PlayerWidth/2-PlayerCoordinates[0])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[1][1]+Settings.PlayerHeight/2)*Settings.PixelsPerUnit+500, (Platforms[1][2]-Settings.PlayerWidth)*Settings.PixelsPerUnit, Settings.FloorHeight*Settings.PixelsPerUnit))
+        pygame.draw.rect(intermediatescreen, (60, 50, 50), ((Platforms[0][0]+Settings.PlayerWidth/2-PlayerCoordinates[0])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[0][1]+Settings.PlayerHeight/2)*Settings.PixelsPerUnit+500, (Platforms[0][2]-Settings.PlayerWidth)*Settings.PixelsPerUnit, Settings.FloorHeight*Settings.PixelsPerUnit))
+        pygame.draw.rect(intermediatescreen, (50, 35, 35), ((Platforms[1][0]+Settings.PlayerWidth/2-PlayerCoordinates[0])*Settings.PixelsPerUnit+750, (PlayerCoordinates[1]-Platforms[1][1]+Settings.PlayerHeight/2)*Settings.PixelsPerUnit+500, (Platforms[1][2]-Settings.PlayerWidth)*Settings.PixelsPerUnit, Settings.FloorHeight*Settings.PixelsPerUnit))
 
         # Draws player
-        if Settings.SpeedAnimation == 1:
+        if Settings.SpeedAnimation != 0:
             PlayerCharacter.set_alpha(40)
-            intermediatescreen.blit(PlayerCharacter, (750-Settings.PlayerWidth*Settings.PixelsPerUnit/2-Momentum[0]*Settings.PixelsPerUnit*0.3, 500-Settings.PlayerHeight*Settings.PixelsPerUnit/2+Momentum[1]*Settings.PixelsPerUnit*0.3))
+            intermediatescreen.blit(PlayerCharacter, (750-Settings.PlayerWidth*Settings.PixelsPerUnit/2-Momentum[0]*Settings.PixelsPerUnit*3*Settings.SpeedAnimation, 500-Settings.PlayerHeight*Settings.PixelsPerUnit/2+Momentum[1]*Settings.PixelsPerUnit*3*Settings.SpeedAnimation))
             PlayerCharacter.set_alpha(110)
-            intermediatescreen.blit(PlayerCharacter, (750-Settings.PlayerWidth*Settings.PixelsPerUnit/2-Momentum[0]*Settings.PixelsPerUnit/5, 500-Settings.PlayerHeight*Settings.PixelsPerUnit/2+Momentum[1]*Settings.PixelsPerUnit/5))
+            intermediatescreen.blit(PlayerCharacter, (750-Settings.PlayerWidth*Settings.PixelsPerUnit/2-Momentum[0]*Settings.PixelsPerUnit*2*Settings.SpeedAnimation, 500-Settings.PlayerHeight*Settings.PixelsPerUnit/2+Momentum[1]*Settings.PixelsPerUnit*2*Settings.SpeedAnimation))
             PlayerCharacter.set_alpha(190)
-            intermediatescreen.blit(PlayerCharacter, (750-Settings.PlayerWidth*Settings.PixelsPerUnit/2-Momentum[0]*Settings.PixelsPerUnit/10, 500-Settings.PlayerHeight*Settings.PixelsPerUnit/2+Momentum[1]*Settings.PixelsPerUnit/10))
+            intermediatescreen.blit(PlayerCharacter, (750-Settings.PlayerWidth*Settings.PixelsPerUnit/2-Momentum[0]*Settings.PixelsPerUnit*Settings.SpeedAnimation, 500-Settings.PlayerHeight*Settings.PixelsPerUnit/2+Momentum[1]*Settings.PixelsPerUnit*Settings.SpeedAnimation))
             PlayerCharacter.set_alpha(255)
         intermediatescreen.blit(PlayerCharacter, (750-Settings.PlayerWidth*Settings.PixelsPerUnit/2, 500-Settings.PlayerHeight*Settings.PixelsPerUnit/2))
-                # TODO: toggleable animation
 
 
 
         finalscreen.blit(intermediatescreen, (0, 0))
         pygame.display.update()
 except:
-    tempa = [TotalTime, Score, Deaths, vars(Settings).values()]
-    open("jsons/results.json", "a").write(str(tempa)+"\n")
-    sys.exit()
+    # print(vars(Settings).values())
+    # print(1/0)
+    try:
+        open("Records/"+Settings.OutputFile, "x")
+    except:
+        pass
+    tempa = str(vars(Settings).values())
+    tempb = [TotalTime, Score, Deaths, tempa[13:len(tempa)-2]]
+    tempb = str(tempb).replace("'", "").replace('"', "").replace(", ", ",")
+    tempb = tempb[1:len(tempb)-1]
+    open("Records/"+Settings.OutputFile, "a").write(str(tempb)+"\n")
